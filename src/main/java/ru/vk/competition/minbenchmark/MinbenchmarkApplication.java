fda3c75e2807e91ad79911373f22d401d82b8c55
Eugene Gusev <egusev@gmail.com>
2022-08-13 01:33:33 +0300

    added requests logging

--- src/main/java/ru/vk/competition/minbenchmark/MinbenchmarkApplication.java
+++ src/main/java/ru/vk/competition/minbenchmark/MinbenchmarkApplication.java
@@ -2,9 +2,23 @@ package ru.vk.competition.minbenchmark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
+@Configuration
 public class MinbenchmarkApplication {
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(false);
+        loggingFilter.setIncludeQueryString(false);
+        loggingFilter.setIncludePayload(false);
+        loggingFilter.setMaxPayloadLength(64000);
+        return loggingFilter;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MinbenchmarkApplication.class, args);
