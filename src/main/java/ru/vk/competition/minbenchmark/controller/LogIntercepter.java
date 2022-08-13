ba8f5d20133ad47cb5c48703b6e2cb112bef5174
Eugene Gusev <egusev@gmail.com>
2022-08-13 15:57:55 +0300

    cleaned up

--- src/main/java/ru/vk/competition/minbenchmark/controller/LogIntercepter.java
+++
@@ -1,7 +0,0 @@
-package ru.vk.competition.minbenchmark.controller;
-
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
-public class LogIntercepter extends HandlerInterceptorAdapter {
-
-}
